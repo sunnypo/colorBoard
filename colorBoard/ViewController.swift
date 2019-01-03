87af0c59428eeb4bdc10d8bc2140ebf8bea90607
Sunny Huang <sunny208@me.com>
2019-01-03 09:09:57 -0500

    Initial Commit

---
+++ colorBoard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  colorBoard
+//
+//  Created by 黄思源 on 2019/1/3.
+//  Copyright © 2019 supencil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
