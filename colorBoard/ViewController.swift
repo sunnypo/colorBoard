86a7d92284450873046d4911f8f6a81d3abb585d
Sunny Huang <sunny208@me.com>
2019-01-05 23:53:33 -0500

    Create a view at the Mainstoryboard Create two switch and two slider to control the color

--- colorBoard/ViewController.swift
+++ colorBoard/ViewController.swift
@@ -9,12 +9,68 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBAction func reset(_ sender: UIButton) {
+        greenSlider.value = 1
+        redSlider.value = 1
+        blueSlider.value = 1
+        greenSwitch.isOn = false
+        redSwitch.isOn = false
+        blueSwitch.isOn = false
+        updateControls()
+    }
+    @IBAction func sliderChanged(_ sender: UISlider) {
+        updateColor()
+        updateControls()
+    }
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    @IBOutlet weak var greenSwitch: UISwitch!
+    @IBOutlet weak var redSwitch: UISwitch!
+    @IBOutlet weak var blueSwitch: UISwitch!
+    
+    @IBAction func colorViewUISwitch(_ sender: UISwitch) {
+        updateColor()
+        updateControls()
+    }
+    
+    func updateControls() {
+        redSlider.isEnabled = redSwitch.isOn
+        greenSlider.isEnabled = greenSwitch.isOn
+        blueSlider.isEnabled =  blueSwitch.isOn
+    }
+    
+    @IBOutlet weak var colorView: UIView!
+    
+    func updateColor() {
+        var red: CGFloat = 0
+        var green: CGFloat = 0
+        var blue: CGFloat = 0
+        if redSwitch.isOn{
+            red = CGFloat(redSlider.value)
+        }
+        if greenSwitch.isOn{
+            green = CGFloat(greenSlider.value)
+        }
+        if blueSwitch.isOn{
+            blue = CGFloat(blueSlider.value)
+        }
+        
+        let color = UIColor(red: red, green: green, blue: blue, alpha: 1)
+        colorView.backgroundColor = color
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateControls()
+        updateColor()
+        
+        colorView.layer.borderWidth = 5
+        colorView.layer.cornerRadius = 20
+        colorView.layer.borderColor = UIColor.white.cgColor
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    
 }
 
